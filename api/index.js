15a26f99ee6a4b7fb3b01df9cffcf6d303a7eee8
minhtuan2000 <minhtuan270820000@gmail.com>
2019-11-16 00:39:31 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,61 +1,41 @@
 module.exports = async (req, res) => {
-  const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+  const axios = require('axios');
   const { body } = req;
   
   // Request
   if (body.type === "Request"){
-    //Send a POST request to the server to analyse the video
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://35.225.126.232/api/link", true);
-    xhr.setRequestHeader('Content-type', 'application/json');
-    //console.log(JSON.stringify({url: tabUrl}));
-    xhr.send(JSON.stringify({clientID: body.clientID, 
-                              url: body.url, 
-                              isBasic: body.isBasic, 
-                              n: body.n, 
-                              l: (body.automode == 0)? body.l: -1, 
-                              offset: body.offset, 
-                              from: body.from,
-                              to: body.to}));
-    xhr.onreadystatechange = function() {
-      //console.log(xhr.readyState);
-      //console.log(xhr.status);
-      if (xhr.readyState == 4 && xhr.status == 200) {
-          res.status(200).send(xhr.responseText);
-      }
-    }
+    axios.post('http://35.225.126.232/api/link', {
+      clientID: body.clientID, 
+      url: body.url, 
+      isBasic: body.isBasic, 
+      n: body.n, 
+      l: (body.automode == 0)? body.l: -1, 
+      offset: body.offset, 
+      from: body.from,
+      to: body.to
+    })
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(200).send(error);
+    });
   }
   
   // Report
   if (body.type === "Report"){
-    const axios = require('axios');
-
     axios.post('http://35.225.126.232/api/report', {
       clientID: body.clientID, 
       email: body.email,
       url: body.url,
       message: body.message
     })
-    .then((res) => {
-      console.log(`statusCode: ${res.statusCode}`);
-      res.send(res);
+    .then((response) => {
+      res.status(200).send(response);
     })
     .catch((error) => {
-      console.error(error);
-    })
-    // //Send a POST request with the report message
-    // let xhr = new XMLHttpRequest();
-    // xhr.open("POST", "http://35.225.126.232/api/report", true);
-    // xhr.setRequestHeader('Content-type', 'application/json');
-    
-    // xhr.send(JSON.stringify());
-    // xhr.onreadystatechange = function() {
-    //   //console.log(xhr.readyState);
-    //   //console.log(xhr.status);
-    //   if (xhr.readyState == 4 && xhr.status == 200) {
-    //       res.status(200).send(xhr.responseText);
-    //   }
-    // }
+      res.status(200).send(error);
+    });
   }
 
   // PurchaseID
