299e63939d23bc0fe7e506b75ff351d93dbc4889
minhtuan2000 <minhtuan270820000@gmail.com>
2019-11-15 23:19:45 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,23 @@
 module.exports = async (req, res) => {
-    const { body } = req
-    res.end(`Hello ${body.name}, you just parsed the request body!`)
+    const { body } = req;
+    //Send a POST request to the server to analyse the video
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://35.225.126.232/api/link", true);
+    xhr.setRequestHeader('Content-type', 'application/json');
+    //console.log(JSON.stringify({url: tabUrl}));
+    xhr.send(JSON.stringify({clientID: body.clientID, 
+                              url: body.tabUrl, 
+                              isBasic: body.isBasic, 
+                              n: body.n, 
+                              l: (body.automode == 0)? body.l: -1, 
+                              offset: body.offset, 
+                              from: body.from,
+                              to: body.to}));
+    xhr.onreadystatechange = function() {
+      //console.log(xhr.readyState);
+      //console.log(xhr.status);
+      if (xhr.readyState == 4 && xhr.status == 200) {
+          res.status(200).send(xhr.responseText);
+      }
+    }
   }
