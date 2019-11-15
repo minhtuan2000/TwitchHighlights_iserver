241a30384a6df4f973039f70fb0ec1cdcc14fd8d
minhtuan2000 <minhtuan270820000@gmail.com>
2019-11-15 23:50:21 +0100

    update files

---
+++ api/index.js
@@ -0,0 +1,33 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+module.exports = async (req, res) => {
+    const { body } = req;
+    
+    // Request
+    if (body.type === "Request"){
+      //Send a POST request to the server to analyse the video
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://35.225.126.232/api/link", true);
+      xhr.setRequestHeader('Content-type', 'application/json');
+      //console.log(JSON.stringify({url: tabUrl}));
+      xhr.send(JSON.stringify({clientID: body.clientID, 
+                                url: body.url, 
+                                isBasic: body.isBasic, 
+                                n: body.n, 
+                                l: (body.automode == 0)? body.l: -1, 
+                                offset: body.offset, 
+                                from: body.from,
+                                to: body.to}));
+      xhr.onreadystatechange = function() {
+        //console.log(xhr.readyState);
+        //console.log(xhr.status);
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            res.status(200).send(xhr.responseText);
+        }
+      }
+    }
+    
+    // Report
+    // PurchaseID
+    // UpdatedStatus
+  }
