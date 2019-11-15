b9859edf86cf5e1cf17b95246b3247acd347b2ea
minhtuan2000 <minhtuan270820000@gmail.com>
2019-11-15 14:47:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.use(express.json());
-// app.use('/api/link', require('./routers/api/link'));
-// app.use('/api/report', require('./routers/api/report'));
-// app.use('/api/purchase', require('./routers/api/purchase'));
-// app.use('/api/status', require('./routers/api/status'));
-// app.use('/terms', require('./routers/api/terms'));
-// app.use('/privacy', require('./routers/api/privacy'));
-
-app.get('/*', function (req, res) {
-    res.send("Hello!");
-});
-
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+module.exports = (req, res) => {
+    const { name = 'World' } = req.query
+    res.send(`Hello ${name}!`)
+}
